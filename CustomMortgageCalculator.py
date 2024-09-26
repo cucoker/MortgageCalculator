e101b02b5178d3ddf0c1f640405b48bad381b48f
Curtis Coker <curtiscoker@gmail.com>
2024-09-26 12:23:31 -0400

    Added merge conflict change

--- CustomMortgageCalculator.py
+++ CustomMortgageCalculator.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 #from dateutil.relativedelta import relativedelta
 #Adding Comment for source control
+#Adding Comment for merge conflict
 
 class MortageCalculator:
     def __init__(self, loanterms, startdate):
